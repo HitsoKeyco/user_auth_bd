0d9279f12f76058f9c30ae41a649cdb574d4a869
EverChic <everchic.sa@gmail.com>
2023-08-14 00:53:36 -0500

    l

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ require('./models')
 
 const main = async () => {
     try {
-        sequelize.sync();
+        sequelize.sync({force:true});
         console.log("DB connected");
         app.listen(PORT);
         console.log(`Server running on port ${PORT}`);
